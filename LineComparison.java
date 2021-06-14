7e61ba02f44af7a1378373373bc2621d8b2d8a17
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-14 21:49:49 +0530

    Added comparing method to compare the 2 lines

--- LineComparison.java
+++ LineComparison.java
@@ -11,13 +11,13 @@ public class LineComparison {
 		System.out.println("Enter y co-ordinate of second point: ");
 		int y2 = s.nextInt();
 
-		System.out.println("Enter x co-ordinate of third point: ");
+		System.out.println("Enter p co-ordinate of third point: ");
 		int p1 = s.nextInt();
-		System.out.println("Enter y co-ordinate of third point: ");
+		System.out.println("Enter q co-ordinate of third point: ");
 		int q1 = s.nextInt();
-		System.out.println("Enter x co-ordinate of forth point: ");
+		System.out.println("Enter p co-ordinate of forth point: ");
 		int p2 = s.nextInt();
-		System.out.println("Enter y co-ordinate of forth point: ");
+		System.out.println("Enter q co-ordinate of forth point: ");
 		int q2 = s.nextInt();
 
 		Double length_of_line1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
@@ -25,10 +25,12 @@ public class LineComparison {
 		
 		System.out.printf("length of 1st end points are : %.2f %n", length_of_line1);
 		System.out.printf("length of 2nd end points are : %.2f %n", length_of_line2);
-		boolean ans = length_of_line1.equals(length_of_line2);
-		if(ans)
-			System.out.println("Both lines are equal.");
+		int res = length_of_line1.compareTo(length_of_line2);
+		if(res > 0)
+			System.out.println("Length of first end points is greater than length of second end points");
+		else if(res < 0)
+			System.out.println("Length of first end points is less than length of second end points");
 		else
-			System.out.println("Both lines are not equal.");
+			System.out.println("Both lines are equal.");
   }
 }
